6a0960e23280ad476b8a70fee1132dc19572d84f
Dhruv-Chandra <dhruvchandra0401@gmail.com>
2023-08-12 20:14:00 +0530

    ..

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,16 @@
+import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import './Expenses.css';
+import Card from '../Card/Card'
+
+function Expenses(props) {
+    return (
+        <Card className = "expenses">
+            <ExpenseItem expense={props.expense[0]}></ExpenseItem>
+            <ExpenseItem expense={props.expense[1]}></ExpenseItem>
+            <ExpenseItem expense={props.expense[2]}></ExpenseItem>
+            <ExpenseItem expense={props.expense[3]}></ExpenseItem>
+        </Card>
+    )
+}
+
+export default Expenses
